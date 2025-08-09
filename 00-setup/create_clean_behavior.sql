39211a75cecab0ca14c90b6b882166f10061d3f8
Shobhan <shobhanpaul879@gmail.com>
2025-08-09 19:55:54 +0530

    feat(cleaning_behavior): handle NULL/blank brand as 'unknown' + update data quality report

--- 00-setup/create_clean_behavior.sql
+++ 00-setup/create_clean_behavior.sql
@@ -1,4 +1,6 @@
+drop table if exists cleaning_behavior;
 create table cleaning_behavior as select
 event_time,event_type,product_id,category_id,
 case when category_code is null or trim(category_code)='' then 'unknown'
-else category_code end as category_code,brand,price,user_id,user_session from staging_behavior;
+else category_code end as category_code,
+case when brand is null or trim(brand) = '' then 'unknown' else brand end as brand,price,user_id,user_session from staging_behavior;
--- 00-setup/data_quality_report.md
+++ 00-setup/data_quality_report.md
@@ -3,7 +3,7 @@
 3. There is no missing event type.
 4. There is no missing product or category id.
 5. There are 32,314 (was 33,840) truly empty or blanks in category\_code column.
-6. Column brand has no empty rows.
+6. Column brand has 13267 (earlier none of the rows were empty) empty rows.
 7. Price column has no empty rows.
 8. User\_id column has no empty rows
 9. Column user\_session has no empty rows.
